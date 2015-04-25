7dc767be6bba5bfe64bbc044eb05085567889c32
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2015-04-24 21:33:16 -0700

    ARM: dts: msm: add DSI PHY regulators to MDSS DSI node
    Currently, we turn off the DSI ctrl/PHY regulators during suspend as
    part of panel power control API which might cause the regulators to be
    turned off before the DSI controller and PHY is disabled. Hence, add the
    DSI PHY regulators to the MDSS DSI node for multiple chipsets and control
    these regulators as part of DSI clock control API.
    
    Change-Id: I134ad45155605db049088efb5d819e142bafdc12
    Signed-off-by: Padmanabhan Komanduru <pkomandu@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -155,7 +155,6 @@ static int mdss_dsi_panel_power_off(struct mdss_panel_data *pdata)
 {
 	int ret = 0;
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata = NULL;
-	int i = 0;
 
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
@@ -196,20 +195,13 @@ static int mdss_dsi_panel_power_off(struct mdss_panel_data *pdata)
 #endif
 	}
 
-	for (i = DSI_MAX_PM - 1; i >= 0; i--) {
-		/*
-		 * Core power module will be disabled when the
-		 * clocks are disabled
-		 */
-		if (DSI_CORE_PM == i)
-			continue;
-		ret = msm_dss_enable_vreg(
-			ctrl_pdata->power_data[i].vreg_config,
-			ctrl_pdata->power_data[i].num_vreg, 0);
-		if (ret)
-			pr_err("%s: failed to disable vregs for %s\n",
-				__func__, __mdss_dsi_pm_name(i));
-	}
+	ret = msm_dss_enable_vreg(
+		ctrl_pdata->power_data[DSI_PANEL_PM].vreg_config,
+		ctrl_pdata->power_data[DSI_PANEL_PM].num_vreg, 0);
+	if (ret)
+		pr_err("%s: failed to disable vregs for %s\n",
+			__func__, __mdss_dsi_pm_name(DSI_PANEL_PM));
+
 #ifdef CONFIG_ZTEMT_LCD_1080P_R63315_SHARP_IPS_5P0
 		mdelay(10);
 #endif
@@ -222,7 +214,6 @@ static int mdss_dsi_panel_power_on(struct mdss_panel_data *pdata)
 {
 	int ret = 0;
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata = NULL;
-	int i = 0;
 
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
@@ -232,21 +223,13 @@ static int mdss_dsi_panel_power_on(struct mdss_panel_data *pdata)
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
-	for (i = 0; i < DSI_MAX_PM; i++) {
-		/*
-		 * Core power module will be enabled when the
-		 * clocks are enabled
-		 */
-		if (DSI_CORE_PM == i)
-			continue;
-		ret = msm_dss_enable_vreg(
-			ctrl_pdata->power_data[i].vreg_config,
-			ctrl_pdata->power_data[i].num_vreg, 1);
-		if (ret) {
-			pr_err("%s: failed to enable vregs for %s\n",
-				__func__, __mdss_dsi_pm_name(i));
-			goto error;
-		}
+	ret = msm_dss_enable_vreg(
+		ctrl_pdata->power_data[DSI_PANEL_PM].vreg_config,
+		ctrl_pdata->power_data[DSI_PANEL_PM].num_vreg, 1);
+	if (ret) {
+		pr_err("%s: failed to enable vregs for %s\n",
+			__func__, __mdss_dsi_pm_name(DSI_PANEL_PM));
+		return ret;
 	}
 #ifdef CONFIG_ZTEMT_LCD_1080P_R63315_SHARP_IPS_5P0
 		mdelay(40);
@@ -267,8 +250,6 @@ static int mdss_dsi_panel_power_on(struct mdss_panel_data *pdata)
 		udelay(2000);
 #endif
 	}
-
-	i--;
 
 	/*
 	 * If continuous splash screen feature is enabled, then we need to
@@ -291,13 +272,6 @@ static int mdss_dsi_panel_power_on(struct mdss_panel_data *pdata)
 
 	}
 
-error:
-	if (ret) {
-		for (; i >= 0; i--)
-			msm_dss_enable_vreg(
-				ctrl_pdata->power_data[i].vreg_config,
-				ctrl_pdata->power_data[i].num_vreg, 0);
-	}
 	return ret;
 }
 
